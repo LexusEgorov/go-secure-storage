51b9843b213c9125bba69ef66faa78a9bf8f5e29
lexusegorov <alexey.egorov.job@gmail.com>
2025-04-16 21:51:02 +0700

    feat: add config; add logger

--- services/auth/cmd/auth/main.go
+++ services/auth/cmd/auth/main.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"auth/config"
+	"auth/internal/logger"
+)
+
 func main() {
-	//TODO: init config
-	//TODO: init logger
+	cfg := config.MustLoad()
+	logger := logger.Init(cfg.Env)
+
+	logger.Debug("test")
+	logger.Info("test")
+	logger.Warn("test")
+	logger.Fatal("test")
 	//TODO: init app
 	//TODO: run app
 }
